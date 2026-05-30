f866cf84d94f8707e188cac886d355523f3b0346
TWShu <93576008+TWShu@users.noreply.github.com>
2026-05-30 21:23:06 +0800

    Change App component to display 'Hello Scout Map'

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,3 @@
 export default function App() {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "24px"
-      }}
-    >
-      Scout Map OK
-    </div>
-  );
+  return <h1>Hello Scout Map</h1>;
 }
