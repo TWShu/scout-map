770002e9745530ccffda22c816f091469034ff31
TWShu <93576008+TWShu@users.noreply.github.com>
2026-05-30 21:16:44 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import { MapContainer, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-
 export default function App() {
   return (
-    <div style={{ height: "100vh" }}>
-      <MapContainer
-        center={[25.033, 121.5654]}
-        zoom={14}
-        style={{ height: "100%" }}
-      >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-      </MapContainer>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "24px"
+      }}
+    >
+      Scout Map OK
     </div>
   );
 }
