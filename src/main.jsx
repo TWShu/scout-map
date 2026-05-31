12ed00bbaa706f1ea476f9d4d20c39ed7aee79b5
TWShu <93576008+TWShu@users.noreply.github.com>
2026-05-31 17:41:21 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "leaflet/dist/leaflet.css";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+document.body.innerHTML = "<h1>Hello from main.jsx</h1>";
