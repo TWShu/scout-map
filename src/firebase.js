c78d69d7c07a0cb959c4547882674c8a0f16dcfd
TWShu <93576008+TWShu@users.noreply.github.com>
2026-06-01 16:39:16 +0800

    Update Firebase configuration with new credentials

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+apiKey: "AIzaSyBF0gXrD0QT9NuceW1hGIexfCFOFXR37Bk",
+
+ authDomain: "pikmin-map-01.firebaseapp.com",
+
+ projectId: "pikmin-map-01",
+
+ storageBucket: "pikmin-map-01.firebasestorage.app",
+
+ messagingSenderId: "1037927470621",
+
+ appId: "1:1037927470621:web:2731170a48d10be34958f4",
+
+ measurementId: "G-S30KCJ8H5Q"
 };
 
 const app = initializeApp(firebaseConfig);
