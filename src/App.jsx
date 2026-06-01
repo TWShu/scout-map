587afb439d7511d703dfc298cc89babd6466daca
TWShu <93576008+TWShu@users.noreply.github.com>
2026-06-01 08:42:33 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default function App() {
-  return <h1>Hello Scout Map</h1>;
+  return (
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <MapContainer
+        center={[25.033964, 121.564468]}
+        zoom={16}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
+  );
 }
