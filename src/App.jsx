448bae5ee9492f96852b86e60d42e4de435011e2
TWShu <93576008+TWShu@users.noreply.github.com>
2026-06-01 13:40:03 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,74 @@
-import { MapContainer, TileLayer } from "react-leaflet";
+import { useEffect, useState } from "react";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  useMap
+} from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+function FlyToLocation({ position }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (position) {
+      map.flyTo(position, 18);
+    }
+  }, [position, map]);
+
+  return null;
+}
+
 export default function App() {
+  const [position, setPosition] = useState([25.033964, 121.564468]);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition([
+          pos.coords.latitude,
+          pos.coords.longitude
+        ]);
+      },
+      (err) => {
+        console.log(err);
+      },
+      {
+        enableHighAccuracy: true
+      }
+    );
+  }, []);
+
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <MapContainer
-        center={[25.033964, 121.564468]}
-        zoom={16}
+        center={position}
+        zoom={18}
         style={{ width: "100%", height: "100%" }}
       >
         <TileLayer
-          attribution='&copy; OpenStreetMap contributors'
+          attribution="© OpenStreetMap"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+        <FlyToLocation position={position} />
+
+        <Marker position={position}>
+          <Popup>目前位置</Popup>
+        </Marker>
       </MapContainer>
     </div>
   );
